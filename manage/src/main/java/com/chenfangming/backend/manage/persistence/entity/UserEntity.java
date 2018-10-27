775e023c947f88a661688794bab7e5676c5e037b
陈方明 <cfmmail@sina.com>
2018-10-27 21:16:44 +0800

    初始化

--- manage/src/main/java/com/chenfangming/backend/manage/persistence/entity/UserEntity.java
+++ manage/src/main/java/com/chenfangming/backend/manage/persistence/entity/UserEntity.java
@@ -1,6 +1,8 @@
 package com.chenfangming.backend.manage.persistence.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import model.BaseEntity;
 
 /**
  * 用户数据表实体
@@ -8,7 +10,10 @@ import lombok.Data;
  * @since 2018-10-25 20:34
  */
 @Data
-public class UserEntity {
+@EqualsAndHashCode(callSuper = true)
+public class UserEntity extends BaseEntity {
+    /** 序列化id **/
+    private static final long serialVersionUID = 3811879965122773242L;
     private Long id;
     private String name;
 }
