d9f148ca182413295b09677ca7bd771deb6c0cb6
陈方明 <cfmmail@sina.com>
2018-11-10 22:26:11 +0800

    定时任务

---
+++ manage/src/main/java/com/chenfangming/backend/manage/controller/JonController.java
@@ -0,0 +1,12 @@
+package com.chenfangming.backend.manage.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 定时任务控制器
+ * @author 陈方明  cfmmail@sina.com
+ * @since 2018-11-10 20:02
+ */
+@RestController
+public class JonController {
+}
