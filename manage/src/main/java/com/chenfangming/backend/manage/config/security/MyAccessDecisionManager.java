e354a69555053490d643718df93b60143d1a240a
陈方明 <cfmmail@sina.com>
2018-12-12 22:14:52 +0800

    1

--- manage/src/main/java/com/chenfangming/backend/manage/config/security/MyAccessDecisionManager.java
+++ manage/src/main/java/com/chenfangming/backend/manage/config/security/MyAccessDecisionManager.java
@@ -30,14 +30,14 @@ public class MyAccessDecisionManager implements AccessDecisionManager {
      */
     @Override
     public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> collection) throws AccessDeniedException, InsufficientAuthenticationException {
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            throw new InsufficientAuthenticationException(DefaultResponseStatus.NO_AUTHENTICATION_ERROR.getMessage());
+        }
         //  当前资源所有访问的角色列表
         Iterator<ConfigAttribute> canAccessRoleSet = collection.iterator();
         //  当前认证对象所拥有的角色列表
         Collection<? extends GrantedAuthority> currentRoleSet = authentication.getAuthorities();
         while (canAccessRoleSet.hasNext()) {
-            if (authentication instanceof AnonymousAuthenticationToken) {
-                throw new InsufficientAuthenticationException(DefaultResponseStatus.NO_AUTHENTICATION_ERROR.getMessage());
-            }
             //  可以访问的角色
             String canAccess = canAccessRoleSet.next().getAttribute();
             for (GrantedAuthority authority : currentRoleSet) {
