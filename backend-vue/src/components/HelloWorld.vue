7261bb2413eb4b3a2cf9041d01700a6c6fccfef5
陈方明 <cfmmail@sina.com>
2018-12-18 21:49:37 +0800

    vue

---
+++ backend-vue/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    这是哪里啊？
+  </div>
+</template>
+
+<script>
+  // export default {
+  //   name: 'HelloWorld',
+  //   props: {
+  //     msg: String
+  //   }
+  // }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ backend-vue/src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      //  当访问路由时，它是惰性加载的。
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }
+  ]
+})
