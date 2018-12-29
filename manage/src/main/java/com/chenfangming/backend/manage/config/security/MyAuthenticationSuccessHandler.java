88db72c01c2b43ae111c8022bc3559a5143eeda9
陈方明 <cfmmail@sina.com>
2018-12-29 21:28:13 +0800

    1

--- manage/src/main/java/com/chenfangming/backend/manage/config/security/MyAuthenticationSuccessHandler.java
+++ manage/src/main/java/com/chenfangming/backend/manage/config/security/MyAuthenticationSuccessHandler.java
@@ -1,11 +1,14 @@
 package com.chenfangming.backend.manage.config.security;
 
+import com.chenfangming.common.StringHelper;
 import com.chenfangming.common.model.response.DefaultResponseStatus;
 import com.chenfangming.common.model.response.ResponseEntity;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
@@ -19,10 +22,16 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+  /** ObjectMapper. **/
   private ObjectMapper objectMapper;
+  /** ValueOperations. **/
+  private ValueOperations<Object, Object> valueOperations;
+  /** 进入Token. **/
+  public static final String ACCESS_TOKEN = "X-Access-Token";
 
-  public MyAuthenticationSuccessHandler(ObjectMapper objectMapper) {
+  public MyAuthenticationSuccessHandler(ObjectMapper objectMapper, ValueOperations<Object, Object> valueOperations) {
     this.objectMapper = objectMapper;
+    this.valueOperations = valueOperations;
   }
 
   /**
@@ -34,8 +43,11 @@ public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHand
    */
   @Override
   public void onAuthenticationSuccess(HttpServletRequest req, HttpServletResponse resp, Authentication authentication) throws IOException {
+    String uuid = StringHelper.uuid();
+    valueOperations.set("loginUser:" + uuid, authentication, 1000L, TimeUnit.SECONDS);
     resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE);
-    ResponseEntity<String> response = new ResponseEntity<>(DefaultResponseStatus.SUCCESS, "这是登录成功的token");
+    resp.setHeader(ACCESS_TOKEN, uuid);
+    ResponseEntity<String> response = new ResponseEntity<>(DefaultResponseStatus.SUCCESS, uuid);
     resp.getWriter().print(objectMapper.writeValueAsString(response));
     resp.getWriter().flush();
   }
