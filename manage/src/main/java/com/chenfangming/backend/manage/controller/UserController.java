5bd0671820a29f697751acde5500ff82b6dc1280
陈方明 <cfmmail@sina.com>
2018-12-29 18:34:21 +0800

    redis

--- manage/src/main/java/com/chenfangming/backend/manage/controller/UserController.java
+++ manage/src/main/java/com/chenfangming/backend/manage/controller/UserController.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 用户控制器
+ * 用户控制器.
  * @author 陈方明  cfmmail@sina.com
  * @since 2018-12-12 22:19
  */
---
+++ manage/src/main/java/com/chenfangming/backend/manage/persistence/mapper/DirectionaryMapper.java
@@ -0,0 +1,14 @@
+package com.chenfangming.backend.manage.persistence.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 字典表操作.
+ * @author 陈方明  cfmmail@sina.com
+ * @since 2018-12-29 18:32
+ */
+@Mapper
+@Repository
+public interface DirectionaryMapper {
+}
