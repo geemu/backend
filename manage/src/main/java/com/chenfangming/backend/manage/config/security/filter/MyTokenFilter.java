38cb1e10bf863a4010dc6c8fbcbb7a741a3ee81e
陈方明 <cfmmail@sina.com>
2019-01-01 19:45:59 +0800

    redis

--- manage/src/main/java/com/chenfangming/backend/manage/config/security/filter/MyTokenFilter.java
+++ manage/src/main/java/com/chenfangming/backend/manage/config/security/filter/MyTokenFilter.java
@@ -1,13 +1,19 @@
 package com.chenfangming.backend.manage.config.security.filter;
 
-import com.chenfangming.backend.manage.config.security.support.MyUserDetails;
+import com.chenfangming.backend.manage.config.security.support.MySimpleGrantedAuthority;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
@@ -43,12 +49,25 @@ public class MyTokenFilter extends OncePerRequestFilter {
     String accessToken = request.getHeader("X-Access-Token");
     if (null != accessToken) {
       accessToken = "loginUser:" + accessToken;
-
-      MyUserDetails data = (MyUserDetails) redisTemplate.opsForValue().get(accessToken);
-
-      System.out.println(data);
-      //    UsernamePasswordAuthenticationToken data = new UsernamePasswordAuthenticationToken(name, name, authorities);
-      //SecurityContextHolder.createEmptyContext().setAuthentication(data);
+      @SuppressWarnings("unchecked")
+      Map<String, Object> map = (Map) redisTemplate.opsForValue().get(accessToken);
+      if (null == map) {
+        SecurityContextHolder.createEmptyContext();
+      } else {
+        String name = (String) map.getOrDefault("username", "");
+        List list = (List) map.getOrDefault("authorities", Collections.emptyList());
+        HashSet<MySimpleGrantedAuthority> authorities = new HashSet<>();
+        for (Object obj : list) {
+          @SuppressWarnings("unchecked")
+          Map<String, String> item = (Map) obj;
+          String roleId = item.get("authority");
+          if (null != roleId) {
+            authorities.add(new MySimpleGrantedAuthority(roleId));
+          }
+        }
+        UsernamePasswordAuthenticationToken data = new UsernamePasswordAuthenticationToken(name, name, authorities);
+        SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(data);
+      }
     }
     chain.doFilter(request, response);
   }
