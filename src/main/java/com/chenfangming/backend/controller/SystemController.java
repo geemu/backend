576bcbb9be8d324966079293570a70313a1f3e9c
陈方明 <cfmmail@sina.com>
2019-03-27 22:07:33 +0800

    权限

--- src/main/java/com/chenfangming/backend/controller/SystemController.java
+++ src/main/java/com/chenfangming/backend/controller/SystemController.java
@@ -1,11 +1,15 @@
 package com.chenfangming.backend.controller;
 
+import com.chenfangming.backend.core.http.ResponseEntity;
+import com.chenfangming.backend.domain.request.LoginReq;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,6 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @Api(description = "系统控制器")
 public class SystemController {
+
+    @PostMapping("login")
+    public ResponseEntity<String> login(@RequestBody LoginReq req) {
+        log.info("用户登录：{}", req);
+        return null;
+    }
 
     @ApiOperation("获取页面的配置参数")
     @GetMapping("{page:[a-zA-Z]+}")
--- src/main/java/com/chenfangming/backend/domain/request/LoginReq.java
+++ src/main/java/com/chenfangming/backend/domain/request/LoginReq.java
@@ -10,7 +10,7 @@ import lombok.Data;
 @Data
 public class LoginReq {
     /** 用户名 **/
-    private String userName;
+    private String name;
     /** 密码 **/
     private String password;
     /** 验证码标识 **/
